c659100f2b4ae4b98e1aee33f3559d5251b09dba
kfoxb <kfoxb@kfoxb.com>
2017-03-04 11:37:19 -0800

    Create messages SQL table

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,27 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  --  create a new id column
+    -- set it to auto_increment
+    -- set it to int
+    -- set it to primary key
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  -- create a new message column
+    -- set it to varchar(255)
+    -- set it to not null
+  message varchar(255) NOT NULL,
+  -- create a usernames foreign key column
+    -- int
+    -- set foreign key reference to usernames.id
+  FK_usernames_messages int,
+  -- create a  roomnames foreign key column
+    -- int
+    -- set foreign key reference to roomnames.id
+  FK_roomnames_messages int
 );
-
-/* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
